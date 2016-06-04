6cc6c8c88f8a65b25afb8a8251bbbc20343fbd96
vinaypuppal <puppalvinay@gmail.com>
2016-06-05 00:57:56 +0530

    change req.ip to req.connection.remoteAddress

--- helpers/header-parser.js
+++ helpers/header-parser.js
@@ -1,5 +1,5 @@
 module.exports = function(req){
-	var ip = req.ip.split(":").pop()+'\n';
+	var ip = req.connection.remoteAddress.split(":").pop()+'\n';
 	var language = req.acceptsLanguages().toString().split(',')[0]+'\n';
 	var software = req.headers['user-agent'].split('(')[1].split(')')[0];
 	return{
